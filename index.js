76e7e9c8cbcc8eb9742026a4984628ec7ca0e00a
WyvernP <68507963+xWyvernPx@users.noreply.github.com>
2021-08-24 20:55:50 +0700

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+
+    res.send('Hello World!oh');
+});
+app.get("/photo", (req, res) => {
+    res.send("this is photo page");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
